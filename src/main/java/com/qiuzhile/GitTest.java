9e76d7e0075c1b4c93ff8b82da4ac529b48b0b36
qiuzhile <qiuzhile613@163.com>
2024-03-03 19:15:09 +0800

    first commit

---
+++ src/main/java/com/qiuzhile/GitTest.java
@@ -0,0 +1,16 @@
+package com.qiuzhile;
+
+/**
+ * ClassName: GitTest
+ * Package: com.qiuzhile
+ * Description:
+ *
+ * @Author: Zhang Bin
+ * @Create: 2024/3/3 - 19:11
+ * @Version: v1.0
+ */
+public class GitTest {
+    public static void main (String[] args) {
+        System.out.println ("Hello Git!");
+    }
+}
