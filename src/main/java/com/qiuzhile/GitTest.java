a9340cb29846306bb1dd9bb0e5b583c1b7fcf5d9
qiuzhile <qiuzhile613@163.com>
2024-03-03 19:16:54 +0800

    second commit

--- src/main/java/com/qiuzhile/GitTest.java
+++ src/main/java/com/qiuzhile/GitTest.java
@@ -12,5 +12,6 @@ package com.qiuzhile;
 public class GitTest {
     public static void main (String[] args) {
         System.out.println ("Hello Git!");
+        System.out.println ("Hello Git2!");
     }
 }
