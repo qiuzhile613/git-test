25464ccfbb07318ba5b2436441a8451865b86d4d
qiuzhile <qiuzhile613@163.com>
2024-03-03 19:28:25 +0800

    hot-fix commit 1

--- src/main/java/com/qiuzhile/GitTest.java
+++ src/main/java/com/qiuzhile/GitTest.java
@@ -14,5 +14,7 @@ public class GitTest {
         System.out.println ("Hello Git!");
         System.out.println ("Hello Git2!");
         System.out.println ("Hello Git3!");
+        // hot-fix commit1
+        System.out.println ("Hello Git3!");
     }
 }
